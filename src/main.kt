e0aa2228d1c21379f7697f92e08458be5f2dd672
Owi-ti <owittiesther@gmail.com>
2022-02-26 17:10:18 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,33 @@
+fun main(){
+    var text= "akirachix"
+    var Char1= text[0]
+    var Char2= text[2]
+    var Char3= text[3]
+    var results =text[0].toString() +text[2] +text[3]
+    println(results)
+
+    var name="Esther"
+    var age= 20
+    var details="Hi, my name is $name and i am $age years old"
+    println(details)
+
+    var Name = "esther"
+    println(Name.length)
+
+    var naMe = "Esther"
+    if (naMe=="joan") {
+        println("thats me.")
+    } else {
+        println("I don’t know who that is.")
+    }
+
+    var naMe2 = "Esther"
+    if (naMe=="Esther") {
+        println("thats me.")
+    } else {
+        println("I don’t know who that is.")
+    }
+
+
+}
+
